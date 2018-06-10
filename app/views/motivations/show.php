4f342864ca885d0458e94f0645d57838068326a7
Sébastien Pereda <sebastien.pereda@gmail.com>
2018-06-11 01:16:50 +0200

    Ajout lien GitHub

--- app/views/motivations/show.php
+++ app/views/motivations/show.php
@@ -6,6 +6,8 @@
         <a href="/" class="btn btn-outline-dark">Une autre bonne raison</a>
     </div>
     <div class="mt-3 small">
-        <p>Et parce que je n’ai rien à cacher et le code de ce mini-projet est sur <a href="/">GitHub</a></p>
+        <p>Et parce que je n’ai rien à cacher et le code de ce mini-projet est sur
+            <a href="https://github.com/bastosh/inextenso">GitHub</a>
+        </p>
     </div>
 <?php require VIEWS.'partials/footer.php'; ?>
